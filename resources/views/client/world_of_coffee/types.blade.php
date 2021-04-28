3de9624da9d1ecebd33c397129b9be17fafbe896
feryal abdullah <feryalabdullah26@gmail.com>
2021-04-28 23:46:49 +0300

    Modify Coffee type data

--- resources/views/client/world_of_coffee/types.blade.php
+++ resources/views/client/world_of_coffee/types.blade.php
@@ -3,32 +3,39 @@
 
 <div class="timeline-container" id="timeline-1">
     <div class="timeline-header">
-        <h2 class="timeline-header__title">Type of coffee</h2>
+        <h2 class="timeline-header__title">Types of coffee</h2>
         <h3 class="timeline-header__subtitle">WORLD OF COFFEE</h3>
     </div>
     <div class="timeline">
-        <div class="timeline-item" data-text="COFFEE TYPE 1">
-            <div class="timeline__content"><img class="timeline__img" src="http://i.cdn.ensonhaber.com/resimler/diger/ataturk_3473.jpg" />
+        <div class="timeline-item" data-text="Arabica">
+            <div class="timeline__content"><img class="timeline__img" src="{{asset('asset/img/arab.jpg')}}" />
                 <h2 class="timeline__content-title">01</h2>
-                <p class="timeline__content-desc">He was born in 1881 (probably in the spring) in Salonica, then an Ottoman city, now inGreece. His father Ali Riza, a customs official turned lumber merchant, died when Mustafawas still a boy. His mother Zubeyde, adevout and strong-willed woman, raised him and his sister.</p>
+                <p class="timeline__content-desc">Arabica is the most popular type of coffee, hands down. Depending on who you ask, many coffee enthusiasts prefer using Arabica beans due to its taste. Typically used for black coffee,
+                    Arabica beans have a sweeter, more complex flavor that you can drink straight.</p>
             </div>
         </div>
-        <div class="timeline-item" data-text="COFFEE TYPE 2">
-            <div class="timeline__content"><img class="timeline__img" src="http://gazetemanifesto.com/wp-content/uploads/2015/11/mustafa-kemal.jpg" />
+        <div class="timeline-item" data-text="Robusta">
+            <div class="timeline__content"><img class="timeline__img" src="{{asset('asset/img/robusta.jpeg')}}" />
                 <h2 class="timeline__content-title">02</h2>
-                <p class="timeline__content-desc">First enrolled in a traditionalreligious school, he soon switched to a modern school. In 1893, he entered a military highschool where his mathematics teacher gave him the second name Kemal (meaning perfection)in recognition of young Mustafa's superior achievement.</p>
+                <p class="timeline__content-desc">Robusta is cheaper and stronger. Because of its bitter flavor, you’ll typically see Robusta used for
+                    espresso drinks and in instant coffee mixes. If your Monday morning is lagging, reach for a cup of coffee that uses Robusta beans.
+                    Their high caffeine content will wake you right up!</p>
             </div>
         </div>
-        <div class="timeline-item" data-text="FATHER OF THE TURKS">
-            <div class="timeline__content"><img class="timeline__img" src="http://www.volpeypir.com/upload/3732.jpg" />
+        <div class="timeline-item" data-text="Excelsa Coffee Beans">
+            <div class="timeline__content"><img class="timeline__img" src="{{asset('asset/img/excelsa.jpg')}}" />
                 <h2 class="timeline__content-title">03</h2>
-                <p class="timeline__content-desc">In 1905, Mustafa Kemal graduated from the War Academy in Istanbul with the rank of Staff Captain. Posted in Damascus, he started with several colleagues, a clandestinesociety called "Homeland and Freedom" to fight against the Sultan'sdespotism.</p>
+                <p class="timeline__content-desc">Excelsa coffee beans are considered as unique because they are not used quite widely. In fact, they only contribute 7% to the consumption of coffee worldwide.
+                    They are usually grown in Southeast Asia. The flavor that they produce is more fruity and tart. These coffee beans are usually used in blends so as to give the coffee a boost of complexity and flavor.
+                    As such, it is best sampled at the back and middle palate.</p>
             </div>
         </div>
-        <div class="timeline-item" data-text="FATHER OF THE TURKS">
+        <div class="timeline-item" data-text="Liberica Coffee">
             <div class="timeline__content"><img class="timeline__img" src="http://cdn.yemek.com/uploads/2014/11/ataturk-10-kasim.jpg" />
                 <h2 class="timeline__content-title">04</h2>
-                <p class="timeline__content-desc">In 1908 he helped the group of officers who toppled the Sultan. Mustafa Kemal'scareer flourished as he won his heroism in the far corners of the Ottoman Empire,including Albania and Tripoli. He also briefly served as a staff officer in Salonica andIstanbul and as a military attache in Sofia.</p>
+                <p class="timeline__content-desc">Liberica Coffee is one that is specific to the Philippines. They have a fruity and floral aroma, producing coffee with a smoky, woody flavor with a full body. 
+                The reason why Liberica beans were imported from the Philippines to the U.S. was the lack of Arabica beans back in 1890. Eventually, the U.S. stopped the trade until the Philippines claimed their independence. 
+                It is quite challenging to secure Liberica coffee these days, but it did play a huge role in the history of the coffee world. In terms of appearance, Liberica coffee beans are bigger than others, usually asymmetrical in shape. </p>
             </div>
         </div>
         <div class="timeline-item" data-text="FATHER OF THE TURKS">
@@ -36,55 +43,8 @@
                 <h2 class="timeline__content-title">05</h2>
                 <p class="timeline__content-desc">In 1915, when Dardanelles campaign was launched, Colonel Mustafa Kemal became anational hero by winning successive victories and finally repelling the invaders.</p>
             </div>
-        </div>
-        <div class="timeline-item" data-text="FATHER OF THE TURKS">
-            <div class="timeline__content"><img class="timeline__img" src="http://blog.istanbul1881.com/wp-content/uploads/2016/08/atat%C3%BCrk-%C3%BCn-inan%C4%B1lmaz-karizmatik-fotograf%C4%B1_861050.jpg" />
-                <h2 class="timeline__content-title">06</h2>
-                <p class="timeline__content-desc">Promotedto general in 1916, at age 35, he liberated two major provinces in eastern Turkey thatyear. In the next two years, he served as commander of several Ottoman armies inPalestine, Aleppo, and elsewhere, achieving another major victory by stopping the enemyadvance at Aleppo.</p>
-            </div>
-        </div>
-        <div class="timeline-item" data-text="FATHER OF THE TURKS">
-            <div class="timeline__content"><img class="timeline__img" src="http://manisanokta.com/wp-content/uploads/2014/07/ataturk-20.jpg" />
-                <h2 class="timeline__content-title">07</h2>
-                <p class="timeline__content-desc">On May 19, 1919, Mustafa Kemal Pasha landed in the Black Sea port of Samsun to startthe War of Independence. In defiance of the Sultan's government, he rallied a liberationarmy in Anatolia and convened the Congress of Erzurum and Sivas which established thebasis for the new national effort under his leadership.</p>
-            </div>
-        </div>
-        <div class="timeline-item" data-text="FATHER OF THE TURKS">
-            <div class="timeline__content"><img class="timeline__img" src="http://www.volpeypir.com/upload/3732.jpg" />
-                <h2 class="timeline__content-title">08</h2>
-                <p class="timeline__content-desc">
-                    On April 23, 1920, the GrandNational Assembly was inaugurated. Mustafa Kemal Pasha was elected to its Presidency.
-                    Fighting on many fronts, he led his forces to victory against rebels and invadingarmies. Following the Turkish triumph at the two major battles at Inonu in Western Turkey,the Grand National Assembly conferred on Mustafa Kemal Pasha the title ofCommander-in-Chief with the rank of Marshal.
-                </p>
-            </div>
-        </div>
-        <div class="timeline-item" data-text="FATHER OF THE TURKS">
-            <div class="timeline__content"><img class="timeline__img" src="http://sanatkaravani.com/wp-content/uploads/2015/11/ataturk-4.jpg" />
-                <h2 class="timeline__content-title">09</h2>
-                <p class="timeline__content-desc">At the end of August 1922, the Turkish armieswon their ultimate victory. Within a few weeks, the Turkish mainland was completelyliberated, the armistice signed, and the rule of the Ottoman dynasty abolished</p>
-            </div>
-        </div>
-        <div class="timeline-item" data-text="FATHER OF THE TURKS">
-            <div class="timeline__content"><img class="timeline__img" src="https://turkcetarih.com/wp-content/uploads/2015/05/Atat%C3%BCrkveCumhuriyet.jpg" />
-                <h2 class="timeline__content-title">10</h2>
-                <p class="timeline__content-desc">In July 1923, the national government signed the Lausanne Treaty with Great Britain,France, Greece, Italy, and others. In mid-October, Ankara became the capital of the new Turkish State. On October 29, the Republic was proclaimed and Mustafa Kemal Pasha wasunanimously elected President of the Republic.</p>
-            </div>
-        </div>
-        <div class="timeline-item" data-text="FATHER OF THE TURKS">
-            <div class="timeline__content"><img class="timeline__img" src="https://pbs.twimg.com/media/Cw69H8pXUAEaSqa.jpg" />
-                <h2 class="timeline__content-title">11</h2>
-                <p class="timeline__content-desc">
-                    The account of Atatürk's fifteen year Presidency is a saga of dramatic modernization.With indefatigable determination, he created a new political and legal system, abolished the Caliphate and made both government and education secular, gave equal rights to women,changed the alphabet and the attire, and advanced the arts and the sciences, agricultureand industry.
-                    In 1934, when the surname law was adopted, the national parliament gave him the name <i><strong>"Atatürk" (Father of the Turks)</strong></i>
-                </p>
-            </div>
-        </div>
-        <div class="timeline-item" data-text="FATHER OF THE TURKS">
-            <div class="timeline__content"><img class="timeline__img" src="http://www.bik.gov.tr/wp-content/uploads/2016/11/20161110_2_20037273_15856882.jpg?fit=1024%2C863" />
-                <h2 class="timeline__content-title">12</h2>
-                <p class="timeline__content-desc">On November 10, 1938, following an illness of a few months, the national liberator and the Father of modern Turkey died. But his legacy to his people and to the world endures.</p>
-            </div>
-        </div>
+        </div>      
+       
     </div>
 </div>
 
