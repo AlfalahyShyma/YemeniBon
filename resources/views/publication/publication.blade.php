788c2a625298b6b8a7b602f692ba012a0e96b99c
Alfalahy_Shyma <alfalahyshyma@gmail.com>
2021-05-02 14:28:46 +0300

    edit publication bg

--- resources/views/publication/publication.blade.php
+++ resources/views/publication/publication.blade.php
@@ -1,5 +1,17 @@
 @include('client/header')
-
+<div class='blurImg'>
+    <!-- style="background-image: url('https://images.unsplash.com/photo-1447933601403-0c6688de566e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=656&q=80')" -->
+    <div class="page-bg"></div>
+    <div class='blur' style="background-image: url('https://d262ilb51hltx0.cloudfront.net/fit/c/1600/1280/gradv/29/81/40/darken/50/blur/50/1*4ncz3hLxmL8E_bUh-0z62w.jpeg')"></div>
+</div>
+<!-- style="background-color: #e3c47a;" -->
+<header class="pages-header">
+    <div>
+        <!-- <img class="project-img" src="{{asset('asset/img/pro1.jpg')}}" alt="" srcset=""> -->
+        
+    </div>
+</header>
+<br>
 <main id="content" class="main">
     <section id="publication-wrrapper" class="content">
 
